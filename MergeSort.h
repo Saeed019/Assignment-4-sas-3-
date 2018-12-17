97565e14f84a927242ec1dd9f070e055a40a4098
abusa <abusa@DESKTOP-1IOICRI>
2018-12-18 00:44:15 +0600

    header file of merge sort is added.

---
+++ MergeSort.h
@@ -0,0 +1,13 @@
+#ifndef MERGESORT_H_INCLUDED
+#define MERGESORT_H_INCLUDED
+
+class MergeSort
+{
+public:
+    MergeSort();
+    ~MergeSort();
+    void merge_sort(int value[],int startIndex, int endIndex);
+};
+#endif // MERGESORT_H_INCLUDED
+
+
