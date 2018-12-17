5dbc5e0d9e8fdd90b3a627292b984361f1456d4f
abusa <abusa@DESKTOP-1IOICRI>
2018-12-17 23:52:19 +0600

    Insertion sort header file added successfully.

---
+++ InsertionSort.cpp
@@ -0,0 +1,34 @@
+#include "InsertionSort.h"
+#include <iostream>
+using namespace std;
+InsertionSort::InsertionSort()
+{
+}
+InsertionSort::~InsertionSort()
+{
+}
+void InsertionSort::InsertionSorter(int arr[], int arr_size)
+{
+     cout <<"InsertionSort :"<<endl;
+    if(arr_size > 1)
+    {
+        int size = arr_size;
+        for(int index = 1; index < size; ++index)
+        {
+            int index1 = index - 1;
+            int key = arr[index];
+            while(index1 >= 0 && arr[index1] > key)
+            {
+                arr[index1+1] = arr[index1];
+                --index1;
+            }
+            arr[index1+1] = key;
+            for(int index2 = 0; index2 < arr_size; ++index2)
+    {
+        cout << arr[index2]<<" ";
+    }
+    cout <<endl;
+        }
+
+    }
+}
